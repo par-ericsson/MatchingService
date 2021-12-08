cae1c80da84bd591dc720818d15eb61fa838ff6f
Pär Ericsson <ericsson@home.se>
2021-12-08 19:47:05 +0100

    Added repository, automapper with configurations

---
+++ API/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Extensions
+{
+    // An extension method of DateTime class to calculate age
+    public static class DateTimeExtensions
+    {
+        public static int CalculateAge(this DateTime dob)
+        {
+            var today = DateTime.Today;
+            var age = today.Year - dob.Year;
+
+            if (dob.Date > today.AddYears(-age))
+            {
+                age--;
+            }
+
+            return age;
+        }
+    }
+}
