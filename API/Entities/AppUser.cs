43f3adb961a08c6fd6145db9240f6b9e0fd5b0df
Pär Ericsson <ericsson@home.se>
2022-01-01 20:04:31 +0100

    Added a like feature

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -24,6 +24,13 @@ namespace API.Entities
         public string Country { get; set; }
         public ICollection<Photo> Photos { get; set; } = new List<Photo>();
 
+        // This is the list of users that liked the currently logged in user
+        public ICollection<UserLike> LikedByUsers { get; set; }
+
+        // This is the list of users that the currently logged in user has liked
+        public ICollection<UserLike> LikedUsers { get; set; }
+
+
         /* public int GetAge()
         {
             return DateOfBirth.CalculateAge();
